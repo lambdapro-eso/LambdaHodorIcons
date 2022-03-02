e68a71c07be53a4afee1dc574384bd3b87204bc3
Christopher Davenport <chris@christopherdavenport.tech>
2022-03-02 15:45:08 -0800

    Initial Commit

---
+++ users/av/av.lua
@@ -0,0 +1,6 @@
+-- 2022
+
+local u = HodorReflexes.users
+local a = HodorReflexes.anim.users
+
+u["@LambdaPro"] = {"LambdaPro", "|c1b9418L|r|c64b85fa|r|c7cc477m|r|cc5e7beb|r|cf5d5e0d|r|ce1a6afa|r|cb9474dP|r|ca5181cr|r|ca5181co|r"}
