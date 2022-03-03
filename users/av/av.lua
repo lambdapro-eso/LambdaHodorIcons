f5309d5df1db7243e1891ac38a1e0eddbe1fbd8b
Christopher Davenport <chris@christopherdavenport.tech>
2022-03-02 22:36:58 -0800

    Updated Icon and Build

--- users/av/av.lua
+++ users/av/av.lua
@@ -3,4 +3,5 @@
 local u = HodorReflexes.users
 local a = HodorReflexes.anim.users
 
-u["@LambdaPro"] = {"LambdaPro", "|c1b9418L|r|c64b85fa|r|c7cc477m|r|cc5e7beb|r|cf5d5e0d|r|ce1a6afa|r|cb9474dP|r|ca5181cr|r|ca5181co|r"}
+u["@LambdaPro"] = {"LambdaPro", "|cff1b0dL|r|cff361aa|r|cff5026m|r|cff6a33b|r|cff8540d|r|cff9f4da|r|cffb959P|r|cffd466r|r|cffee73o|r"}
+a["@LambdaPro"] = {'HodorReflexes/users/animated/pikadab.dds', 7, 7, 20}
